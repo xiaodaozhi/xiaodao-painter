3754907e3ae748fe82389372523dfaea499a3130
Leeqwind <leeqwind@live.com>
2026-08-04 15:20:58 +0800

    refactor(whiteboard/toolbar): 移除未使用的watch导入
    删除了组件中没有用到的vue watch导入项，精简依赖

--- src/components/whiteboard/Toolbar.vue
+++ src/components/whiteboard/Toolbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { useToolsStore } from './stores/tools'
 import { useCanvasStore } from './stores/canvas'
 import type { ToolType } from './types'
